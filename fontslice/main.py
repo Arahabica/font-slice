8f7b70b57af6cb03913d6421a5c285e03eca0fee
Arahabica <r.sasage@gmail.com>
2020-02-18 03:14:30 +0900

    slice 11 subsets when text file is given.

--- fontslice/main.py
+++ fontslice/main.py
@@ -1,4 +1,5 @@
 import os
+import math
 import hashlib
 from pathlib import Path
 from argparse import ArgumentParser
@@ -15,9 +16,16 @@ FONT_FACE_TEMPLATE = """
 }
 """
 
+ANOTHER_SLICE_COUNT = 10
+
+
+def chunk_list(li, num):
+    return [li[i : i + num] for i in range(0, len(li), num)]
+
 
 def _get_unicode_range_hash(unicode_range):
     return hashlib.sha256(unicode_range.encode()).hexdigest()[:32]
+
 
 def get_120_unicode_ranges():
     with open(os.path.join(os.path.dirname(__file__), "unicode_ranges_120.txt")) as f:
@@ -25,27 +33,47 @@ def get_120_unicode_ranges():
     return [str.strip(line) for line in lines]
 
 
+def convert_unicode_range(char_num_ranges):
+    ranges = []
+    for (f, t) in char_num_ranges:
+        if f == t:
+            ranges.append("U+%x" % f)
+        else:
+            ranges.append("U+%x-%x" % (f, t))
+    return ",".join(ranges)
+
+
 def get_unicode_ranges_from_text(text):
     chars = list(text)
     char_num_list = [ord(char) for char in chars]
     char_num_list = list(set(char_num_list))
     char_num_list.sort()
-    main_unicode_range = ",".join(["U+%x" % num for num in char_num_list])
+    char_num_ranges = []
+    cursor = char_num_list[0]
+    for i, num in enumerate(char_num_list):
+        if i > 0:
+            if num > char_num_list[i - 1] + 1:
+                char_num_ranges.append((cursor, char_num_list[i - 1]))
+                cursor = num
+    char_num_ranges.append((cursor, char_num_list[-1]))
 
-    another_list = []
+    main_unicode_range = convert_unicode_range(char_num_ranges)
+
+    another_ranges = []
     cursor = 1
     for num in char_num_list:
         if num > cursor:
-            another_list.append((cursor, num - 1))
+            another_ranges.append((cursor, num - 1))
         cursor = num + 1
-    another_ranges = []
-    for (f, t) in another_list:
-        if f == t:
-            another_ranges.append("U+%x" % f)
-        else:
-            another_ranges.append("U+%x-%x" % (f, t))
-    another_unicode_range = ",".join(another_ranges)
-    return [main_unicode_range, another_unicode_range]
+
+    chunk_size = math.floor(len(another_ranges) / ANOTHER_SLICE_COUNT)
+    chunked_another_ranges = chunk_list(another_ranges, chunk_size)
+
+    unicode_ranges = [main_unicode_range]
+    for chunk in chunked_another_ranges:
+        unicode_ranges.append(convert_unicode_range(chunk))
+
+    return unicode_ranges
 
 
 def generate_subset(unicode_range, flavor, font_file, output_dir):
@@ -71,7 +99,13 @@ def generate_subset(unicode_range, flavor, font_file, output_dir):
     font_path = Path(font_file)
     name = font_path.stem
     unicode_range_hash = _get_unicode_range_hash(unicode_range)
-    outfile = "%s/%s/%s-subset-%s.%s" % (output_dir, FONT_DIR, name, unicode_range_hash, flavor)
+    outfile = "%s/%s/%s-subset-%s.%s" % (
+        output_dir,
+        FONT_DIR,
+        name,
+        unicode_range_hash,
+        flavor,
+    )
     save_font(font, outfile, options)
     font.close()
 
